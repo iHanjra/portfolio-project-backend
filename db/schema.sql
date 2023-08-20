3fb7d018d564288a9960baa20675bf44d1eb5b8d
Irfan Hanjra <ihanjra@gmail.com>
2023-08-20 18:00:32 -0400

    Added created_at column to table

--- db/schema.sql
+++ db/schema.sql
@@ -14,5 +14,6 @@ CREATE TABLE shirts (
     price DEC(7, 2),
     store TEXT,
     is_favorite BOOLEAN DEFAULT false,
-    image TEXT DEFAULT 'https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C51Q6Wb%2BEakL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_SX679._SX._UX._SY._UY_.png'
+    image TEXT DEFAULT 'https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C51Q6Wb%2BEakL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_SX679._SX._UX._SY._UY_.png',
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
