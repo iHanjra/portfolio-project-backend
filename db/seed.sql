8618929702f53db93532a3a00ed55e6829f4bb53
Irfan Hanjra <ihanjra@gmail.com>
2023-08-21 03:54:25 -0400

    Fixed sizes

--- db/seed.sql
+++ db/seed.sql
@@ -3,9 +3,9 @@
 INSERT INTO
     shirts(name, color, size, price, store, is_favorite, image)
 VALUES
-    ('2 Fast 2 Furious', 'Black', 'Medium', 30.79, 'https://paradiseofflowers.net/', false, DEFAULT),
-    ('Ottoman Emperor', 'Black', 'Large', 24.99, 'https://www.redbubble.com/', true, 'https://ih1.redbubble.net/image.1926843994.0604/ssrco,classic_tee,womens,101010:01c5ca27c6,front_alt,square_product,600x600.jpg'),
-    ('A.C.', 'White', 'X-Large', 15.99, 'https://www.balenciaga.com/en-us', true, 'https://pbs.twimg.com/media/Ftd0VzSaAAAVDBM?format=jpg&name=large'),
-    ('Robert Cop 2', 'Light Blue', 'Medium', 28.99, 'https://storehole.bigcartel.com/product/robert-cop-2-t-shirt', false, 'https://assets.bigcartel.com/product_images/214142371/robert-cop-2-main.png?auto=format&fit=max&w=2000'),
-    ('KOJIMA PRODUCTIONS LOGO', 'Black', 'Medium', 25.00, 'https://kojimaproductions-store.com/products/kojima-productions-logo-t-shirt-white?variant=39436934676553', false, 'https://kojimaproductions-store.com/cdn/shop/products/KJKJP1003MT13BL_1_cf2b37be-43ba-40a1-be6d-b09c3ff8a2c4.png?v=1660699701'),
-    ('REZ INFINITE', 'Black', 'Medium', 28.00, 'https://www.iam8bit.com/collections/rez-infinite/products/level01playerformshirtrezinfinite', false, 'https://www.iam8bit.com/cdn/shop/products/ePvub0fFSkO70tc1fX2V_web_storeimage_rezinfinite_foil_tshirt.jpg');
+    ('2 Fast 2 Furious', 'Black', 'M', 30.79, 'https://paradiseofflowers.net/', false, DEFAULT),
+    ('Ottoman Emperor', 'Black', 'L', 24.99, 'https://www.redbubble.com/', true, 'https://ih1.redbubble.net/image.1926843994.0604/ssrco,classic_tee,womens,101010:01c5ca27c6,front_alt,square_product,600x600.jpg'),
+    ('A.C.', 'White', 'XL', 15.99, 'https://www.balenciaga.com/en-us', true, 'https://pbs.twimg.com/media/Ftd0VzSaAAAVDBM?format=jpg&name=large'),
+    ('Robert Cop 2', 'Light Blue', 'M', 28.99, 'https://storehole.bigcartel.com/product/robert-cop-2-t-shirt', false, 'https://assets.bigcartel.com/product_images/214142371/robert-cop-2-main.png?auto=format&fit=max&w=2000'),
+    ('KOJIMA PRODUCTIONS LOGO', 'Black', 'M', 25.00, 'https://kojimaproductions-store.com/products/kojima-productions-logo-t-shirt-white?variant=39436934676553', false, 'https://kojimaproductions-store.com/cdn/shop/products/KJKJP1003MT13BL_1_cf2b37be-43ba-40a1-be6d-b09c3ff8a2c4.png?v=1660699701'),
+    ('REZ INFINITE', 'Black', 'M', 28.00, 'https://www.iam8bit.com/collections/rez-infinite/products/level01playerformshirtrezinfinite', false, 'https://www.iam8bit.com/cdn/shop/products/ePvub0fFSkO70tc1fX2V_web_storeimage_rezinfinite_foil_tshirt.jpg');
